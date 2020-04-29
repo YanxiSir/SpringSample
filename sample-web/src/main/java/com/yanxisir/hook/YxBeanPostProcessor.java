8dc5a8a72c284683da1e27293d623ce7375a1373
YanxiSir <yanxisir@gmail.com>
2020-04-29 23:44:46 +0800

    add spring

--- sample-web/src/main/java/com/yanxisir/hook/YxBeanPostProcessor.java
+++ sample-web/src/main/java/com/yanxisir/hook/YxBeanPostProcessor.java
@@ -1,5 +1,6 @@
 package com.yanxisir.hook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
@@ -10,18 +11,21 @@ import org.springframework.stereotype.Service;
  * @Date: 2020/4/29
  * @Sign: 心中田间，木行水上
  */
+@Slf4j
 @Service
 public class YxBeanPostProcessor implements BeanPostProcessor {
 
     //bean初始化方法调用前被调用
     @Override
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        log.info("YxBeanPostProcessor -> postProcessBeforeInitialization : {} ", s);
         return o;
     }
 
     //bean初始化方法调用后被调用
     @Override
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        log.info("YxBeanPostProcessor -> postProcessAfterInitialization : {} ", s);
         return o;
     }
 }
